bbd9eb7a2712b8f77dd0cd1881ee70d236383da5
shishc9 <343141457@qq.com>
2022-07-22 23:20:31 +0800

    feat: add multi src compile

---
+++ multi-src/main.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "math_function.h"
+
+int main() {
+    std::cout << "main.cxx" << std::endl;
+    std::cout << "pow: " << pow(2, 16) << std::endl;
+    return 0;
+}
---
+++ multi-src/math_function.cxx
@@ -0,0 +1,15 @@
+#include "math_function.h"
+
+int pow(int base, int exponent) {
+    if (base <= 0 || exponent <= 0) {
+        return -1;
+    }
+
+    int result = base;
+
+    for (int i = 1; i < exponent; i++) {
+        result = result * base;
+    }
+
+    return result;
+}
